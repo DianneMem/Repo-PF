7499e6f9ab7cb99ac181afc48cb6241ad1c7e71a
DianneMem <102989674+DianneMem@users.noreply.github.com>
2022-12-06 19:05:01 -0300

    actions y reducer

--- proyecto-final/src/redux/reducer.js
+++ proyecto-final/src/redux/reducer.js
@@ -2,14 +2,41 @@
 
 const initialState = {
   users: [],
-  posts: [],
+  books: [],
 
 };
 
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    default:
-      return state;
+const rootReducer = (state = initialState, {
+  type, payload }) => {
+
+  switch (type) {
+    case 'GET_ALL_BOOKS':
+      return {
+        ...state,
+        books: payload
+      }
+
+    case 'GET_ALL_USERS':
+      return {
+        ...state,
+        users: payload
+
+      }
+    case 'GET_BOOKS_BY_NAME':
+      return {
+        ...state,
+        books: payload
+      }
+case 'GET_BOOK_DETAILS':
+  return{
+    ...state,
+    detailsBook: payload
   }
-};
+  case 'POST_BOOK':
+    return {
+      ...state,
+
+    }
+  }
+}
 export default rootReducer;
