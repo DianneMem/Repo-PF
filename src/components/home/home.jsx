ff58730adf7fd693280e195556ad07fbc40c57c7
Toto <valverdetomas98@gmail.com>
2022-12-08 15:02:14 -0300

    feat/(./searchBar) add and mount SearchBar.jsx component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -13,7 +13,7 @@ import SearchBar from "../searchBar/SearchBar";
 export default function Home() {
   const dispatch = useDispatch();
   const allBooks = useSelector((state) => state.books);
-  const [currentPage,setCurrentPage]=useState(1)
+  const [currentPage, setCurrentPage] = useState(1);
   // let currentPageGlobal = useSelector((state) => state.currentPage);
   const [booksPerPage, setBooksPerPage] = useState(12);
   const [order, setOrder] = useState("");
@@ -21,51 +21,49 @@ export default function Home() {
   const IndexOfFirstBooks = indexOfLastBooks - booksPerPage;
   const currentBooks = allBooks.slice(IndexOfFirstBooks, indexOfLastBooks);
 
-
   const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber)
-    dispatch(setPage(pageNumber))
+    setCurrentPage(pageNumber);
+    dispatch(setPage(pageNumber));
     // setOrder("")
   };
 
   useEffect(() => {
     dispatch(getAllBooks());
-    setCurrentPage(1)
-    dispatch(setPage(1))
+    setCurrentPage(1);
+    dispatch(setPage(1));
   }, [dispatch]);
-
 
   return (
     <>
-    <SearchBar/>
+      <SearchBar />
       <h1>HOME</h1>
       <Link to={`/createproduct`} className={s.det}>
         Create
       </Link>
       <div className={s.cards}>
-      {
-            
-            currentBooks?.map(b=>{
-              return (
-                <div key={b._id} className={s.card}>
-                  <button className={s.favorite}>
-                    <FiHeart />
-                  </button>
-                  <Card
-                    id={b._id}
-                    title={b.title}
-                    image={b.image}
-                    typebook={b.typebook}
-                    price={b.price}
-                    author={b.author}
-                    type={b.typebook}
-                  />
-                </div>
-              );
-                })
-            }
-            <Paginated  booksPerPage={booksPerPage}
-    allBooks={allBooks.length} paginate={paginate}/>   
+        {currentBooks?.map((b) => {
+          return (
+            <div key={b._id} className={s.card}>
+              <button className={s.favorite}>
+                <FiHeart />
+              </button>
+              <Card
+                id={b._id}
+                title={b.title}
+                image={b.image}
+                typebook={b.typebook}
+                price={b.price}
+                author={b.author}
+                type={b.typebook}
+              />
+            </div>
+          );
+        })}
+        <Paginated
+          booksPerPage={booksPerPage}
+          allBooks={allBooks.length}
+          paginate={paginate}
+        />
       </div>
     </>
   );
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,42 +1,45 @@
 import React from "react";
 import { useState } from "react";
-import {useDispatch} from "react-redux"
+import { useDispatch } from "react-redux";
 import { getBooksByName } from "../../redux/actions";
-export default function SearchBar(){
+export default function SearchBar() {
+  const dispatch = useDispatch();
 
-const dispatch= useDispatch()
+  const [name, setName] = useState("");
+  const [reset, setReset] = useState("");
 
-const [name,setName]=useState("")
-const [reset,setReset]=useState("")
-
-
-function handlerInput(e){
-    e.preventDefault()
-    setName(e.target.value)
+  function handlerInput(e) {
+    e.preventDefault();
+    setName(e.target.value);
     //  dispatch(getBooksByName(name))
     //  setReset("")
-}
-function handlerSumbit(e){
-    e.preventDefault()
-    dispatch(getBooksByName(name))
- setName("")
-}
+  }
+  function handlerSumbit(e) {
+    e.preventDefault();
+    dispatch(getBooksByName(name));
+    setName("");
+  }
 
-console.log(name);
+  console.log(name);
 
-
-return(
+  return (
     <div>
       <div>
-
-        
-      <input  type="text"  onChange={e=>handlerInput(e)} 
-       
-        placeholder="Buscar..." value={name}
-      />
-        <button  onClick={e=>{handlerSumbit(e)}} type="submit">Buscar</button>
+        <input
+          type="text"
+          onChange={(e) => handlerInput(e)}
+          placeholder="Buscar..."
+          value={name}
+        />
+        <button
+          onClick={(e) => {
+            handlerSumbit(e);
+          }}
+          type="submit"
+        >
+          Buscar
+        </button>
       </div>
     </div>
-)
-
-} 
+  );
+}
