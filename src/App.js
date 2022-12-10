b6570360c82a8e54cff3f78be1cae46462acf842
Toto <valverdetomas98@gmail.com>
2022-12-10 15:15:13 -0300

    feat/(login) add login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+
+
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/home";
 import Detail from "./components/detail/detail";
 import CreateProduct from './components/createProduct/CreateProduct';
-
+import Login from "./components/login/Login"
 
 import './App.css';
 
@@ -13,6 +15,7 @@ export default function App() {
   <div className="App">
     <Router>
       <Routes>
+      <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/createproduct" element={<CreateProduct/>}/>
         <Route exact path="/detail/:id" element={<Detail/>} />
@@ -21,4 +24,3 @@ export default function App() {
   </div>
   )
 };
-
