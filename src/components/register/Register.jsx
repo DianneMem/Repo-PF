4318cac7944717891a0506028c84bafd3ff24e8f
Gonzalo Aruquipa <gonzao777info@gmail.com>
2022-12-27 09:37:42 -0400

    add styles login, register, confirm, recover

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
-import { createCustomer, createUser } from "../../redux/actions";
-import { Google } from "@mui/icons-material";
-import { Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { createCustomer, createUser, getAllUsers } from "../../redux/actions";
+import { Google, LockClockOutlined, Send } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Register = () => {
   const [errors, setError] = useState({});
@@ -12,7 +21,9 @@ export const Register = () => {
   const navigate = useNavigate();
   const users = useSelector((state) => state.users);
 
-  useEffect(() => {}, [dispatch]);
+  useEffect(() => {
+    dispatch(getAllUsers());
+  }, [dispatch]);
 
   const [input, setInputs] = useState({
     username: "",
@@ -81,112 +92,122 @@ export const Register = () => {
   }
 
   return (
-    <section className="section-reg ">
-      <div className="container">
-        <div className="row justify-content-center  fixed-top ">
-          <div className="col-md-6 text-center mt-4">
-            <h2 className="heading-section">Register</h2>
-          </div>
-        </div>
+    <div>
+      <Grid
+        container
+        component="main"
+        sx={{ height: "100vh", padding: 6 }}
+        className="texts-login"
+      >
+        <Container component="main" maxWidth="md" sx={{ bgcolor: "#ebebeb", borderRadius: 2 }}>
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h2" sx={{color: "#013a63", mb: 1}}>
+              FlyBooks
+            </Typography>
+            <Avatar sx={{ m: 1, bgcolor: "#ff6700" }}>
+              <LockClockOutlined />
+            </Avatar>
+            
+            <Typography component="h1" variant="h5" sx={{ color: "#013a63" }}>
+              Sign Up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={(e)=> handleSubmit(e)}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    autoComplete="username"
+                    onChange={(e) => handleUser(e)}
+                  />
+                </Grid>
+                {errors.username && <p className="danger-p">{errors.username}</p>}
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    onChange={(e) => handleUser(e)}
+                  />
+                </Grid>
+                {errors.email && <p className="danger-p">{errors.email}</p>}
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    onChange={(e) => handleUser(e)}
+                  />
+                </Grid>
+                {errors.password && <p className="danger-p">{errors.password}</p>}
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="confirmation"
+                    label=" Repeat Password"
+                    type="password"
+                    id="confirmation"
+                    autoComplete="new-password"
+                    onChange={(e) => handleUser(e)}
+                  />
+                </Grid>
+                {errors.confirmation && <p className="danger-p">{errors.confirmation}</p>}
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="outlined"
+                sx={{ mt: 3, mb: 2, color: "#013a63", border:1}}
+                endIcon={<Send />}
+              >
+                Sign Up
+              </Button>
 
-        <div className="row justify-content-center  ">
-          <div className="col-md-12 col-lg-10">
-            <div className="wrap d-flex justify-content-center">
-              <div className="login-wrap p-4 p-lg-5 div-log col-lg-5">
-                <div className="d-flex">
-                  <h3 className="mb-4">Sign In</h3>
-                </div>
-                <form
-                  onSubmit={(e) => handleSubmit(e)}
-                  action="#"
-                  className="signin-form"
-                >
-                  {/* username */}
-                  <div className="form-group mb-3">
-                    <label className="label" for="email">
-                      User Name
-                    </label>
-                    <input
-                      onChange={(e) => handleUser(e)}
-                      type="text"
-                      className="form-control"
-                      name="username"
-                      placeholder="Username"
-                      required
-                    />
-                    {errors.username && <p className="danger-p">{errors.username}</p>}
-                  </div>
-                  <div className="form-group mb-3">
-                    <label className="label" for="email">
-                      E-mail
-                    </label>
-                    <input
-                      onChange={(e) => handleUser(e)}
-                      type="email"
-                      className="form-control"
-                      name="email"
-                      placeholder="E-mail"
-                      required
-                    />
-                    {errors.email && <p className="danger-p">{errors.email}</p>}
-                  </div>
-                  <div className="form-group mb-3">
-                    <label className="label" for="password">
-                      Password
-                    </label>
-                    <input
-                      onChange={(e) => handleUser(e)}
-                      type="password"
-                      className="form-control"
-                      name="password"
-                      placeholder="Password"
-                    />
-                    {errors.password && <p className="danger-p">{errors.password}</p>}
-                  </div>
-                  <div className="form-group mb-3">
-                    <label className="confirm" for="passconfirm">
-                      Password Confirmation
-                    </label>
-                    <input
-                      onChange={(e) => handleUser(e)}
-                      type="password"
-                      className="form-control"
-                      name="confirmation"
-                      placeholder="Confirm your password"
-                      required
-                    />
-                    {errors.confirmation && <p className="danger-p">{errors.confirmation}</p>}
-                  </div>
+              <Button 
+              variant="outlined" 
+              fullWidth 
+              sx={{ mb: 2, border: 1, color: "#013a63" }}
+              href="http://localhost:3001/google/signup"
+              >
+                <Google />
+                <Typography sx={{ ml: 1, color: "#013a63" }}>Google</Typography>
+              </Button>
 
-                  <div className="form-group mb-3"></div>
-
-                  <div className="row justify-content-center ">
-                    <div className="form-group text-center">
-                      <button
-                        type="submit"
-                        className="btnl btnl-primary col-lg-10"
-                      >
-                        Sign In
-                      </button>
-                    </div>
-
-                    {/* enlace para google */}
-                    <div className="mt-1  text-center ">
-                      <a
-                        href="http://localhost:3001/google/signup"
-                        className="order-md-last btn btn-outline-danger col-lg-10"
-                      >
-                        <Google />
-                        <Typography sx={{ ml: 1 }}>Google</Typography>
-                      </a>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+              <Grid container justifyContent="flex-end">
+                <Grid item sx={{opacity: 0.7}}>
+                  <Link to={"/login"} variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </Grid>
+    </div>
   );
 };
