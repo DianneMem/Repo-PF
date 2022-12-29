ffc9816e3cce97372475fae87c8c13f2ea4f152e
lucasbeck1 <lucasbeckford1@gmail.com>
2022-12-28 22:18:08 -0300

    feat/ add Modal Form for Product Update

--- src/components/dashCard/dashCard.jsx
+++ src/components/dashCard/dashCard.jsx
@@ -6,6 +6,30 @@ import { getAllBooks, modifyPost } from "../../redux/actions";
 import defaultImage from '../../assets/bookDefault.png';
 import s from './dashCard.module.css';
 
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import InputAdornment from '@mui/material/InputAdornment';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+
+
+
+
 export default function DashCard({id, title, image, typebook, price, author, categorie, editorial, saga, language, gender, year, state, available, deletes, disable }){
 
   // Only First Mayus
@@ -38,6 +62,7 @@ export default function DashCard({id, title, image, typebook, price, author, cat
   
   
   // Local States
+  const [open, setOpen] = useState(false);
   const [isForm, setIsForm] = useState(false);
   const [input, setInput] = useState({
     title: title,
@@ -52,8 +77,36 @@ export default function DashCard({id, title, image, typebook, price, author, cat
     gender: gender
   });
   const [error, setError] = useState({});
+  
   if(isForm) console.log(input);
   if(isForm) console.log('formError', error);
+  if(open) console.log(input);
+  if(open) console.log('formError', error);
+  
+  
+  
+  // Functions
+  
+  function handleOpen(){
+    setInput({
+      title: title,
+      author: author,
+      editorial: editorial,
+      language: language,
+      year: year,
+      state: state,
+      typebook: typebook,
+      price: price,
+      categorie: categorie,
+      gender: gender
+    });
+    // document.getElementById('title').value = title;
+    setOpen(true);
+  };
+  
+  function handleClose(){
+    setOpen(false);
+  };
   
   function switchForm(e){
     e.preventDefault();
@@ -70,10 +123,6 @@ export default function DashCard({id, title, image, typebook, price, author, cat
       gender: gender
     });
     // document.getElementById('title').value = title;
-    // document.getElementById('author').value = author;
-    // document.getElementById('editorial').value = editorial;
-    // document.getElementById('year').value = year;
-    // document.getElementById('price').value = price;
     isForm ? setIsForm(false) : setIsForm(true);
   };
 
@@ -117,6 +166,7 @@ export default function DashCard({id, title, image, typebook, price, author, cat
       [e.target.name]: e.target.value
     }));
   };
+  
   function selectCategorie(e){
     e.preventDefault();
     setInput({
@@ -124,6 +174,7 @@ export default function DashCard({id, title, image, typebook, price, author, cat
       categorie: e.target.value
     });
   };
+  
   function selectGenders(e){
     e.preventDefault();
     if(!input.gender.includes(e.target.value)){
@@ -136,8 +187,22 @@ export default function DashCard({id, title, image, typebook, price, author, cat
         gender:[...input.gender, e.target.value]
       }));
     }
-    document.getElementById('GenderSelector').selectedIndex = 'DEFAULT';
+    // document.getElementById('GenderSelector').selectedIndex = 'DEFAULT';  
   };
+  
+  function selectGendersMUI(e){
+    let {target: { value }} = e;
+    setInput({
+      // On autofill we get a stringified value.
+      ...input,
+      gender: typeof value === 'string' ? value = value.split(',') : value,
+    });
+    setError(validate({
+      ...input,
+      gender: typeof value === 'string' ? value = value.split(',') : value,
+    }));
+  }
+  
   function unselectGenders(e){
     e.preventDefault();
     if(input.gender.includes(e.target.value)){
@@ -151,6 +216,7 @@ export default function DashCard({id, title, image, typebook, price, author, cat
       }));
     };
   };
+  
   function SelectLanguage(e){
     e.preventDefault();
     setInput({
@@ -158,6 +224,7 @@ export default function DashCard({id, title, image, typebook, price, author, cat
       language: e.target.value
     });
   };
+  
   function changeState(e){
     e.preventDefault();
     if(input.state === 'New'){
@@ -172,6 +239,22 @@ export default function DashCard({id, title, image, typebook, price, author, cat
       })
     };
   };
+  
+  function changeStateMUI(e){
+    if(e.target.value === 'virtual'){
+      setInput({
+        ...input,
+        [e.target.name]: e.target.value,
+        state: 'New'
+      })
+    }else{
+      setInput({
+        ...input,
+        [e.target.name]: e.target.value
+      })
+    }
+  };
+  
   function changeType(e){
     e.preventDefault();
     if(input.typebook === 'physical'){
@@ -186,6 +269,7 @@ export default function DashCard({id, title, image, typebook, price, author, cat
       })
     };
   };
+  
   async function modify(e){
     e.preventDefault();
     const inputSend = input;
@@ -193,6 +277,14 @@ export default function DashCard({id, title, image, typebook, price, author, cat
     await dispatch(modifyPost(id, inputSend));
     dispatch(getAllBooks());
     setIsForm(false);
+  };
+  
+  async function modifyMUI(){
+    const inputSend = input;
+    console.log('SEND', inputSend);
+    await dispatch(modifyPost(id, inputSend));
+    dispatch(getAllBooks());
+    handleClose();
   };
   
 
@@ -216,7 +308,6 @@ return(
         </div>
         <div>
           <span>Genders:</span>
-          {/* <span>{input.gender?.join(', ')}</span> */}
           {input.gender?.map(gen => {return(
           <button value={gen} onClick={e => unselectGenders(e)}>{gen}</button>
           )})}
@@ -322,14 +413,174 @@ return(
           {available? (<p>Available: Yes</p>) : (<p>Available: No</p>)}
         </div>
         <div className={s.infoContainer}>
-          {available? (<button value={id} onClick={e => disable(e)} className="btn btn-outline-warning">Disable</button>) : 
-          (<button value={id} onClick={e => disable(e)} className="btn btn-outline-warning">Enable</button>)}
-          <button value={id} onClick={e => deletes(e)} className="btn btn-outline-danger">Delete</button>
-          <button value={id} onClick={e => switchForm(e)} className="btn btn-outline-primary">Modify</button>
+          {available? (<button value={id} onClick={e => disable(e)}>Disable</button>) : 
+          (<button value={id} onClick={e => disable(e)}>Enable</button>)}
+          <button value={id} onClick={e => deletes(e)}>Delete</button>
+          {/* <button value={id} onClick={e => switchForm(e)}>Modify</button> */}
+          <button onClick={e => handleOpen(e)}>FORM</button>
         </div>
       </div>
     </div>
     )}
+    
+    {/* <Modal open={open}>
+      <>
+      <h1>Hola</h1>
+      <button onClick={e => handleClose(e)}>CLOSE</button>
+      </>
+    </Modal> */}
+    
+    <Dialog open={open} onClose={handleClose} maxWidth="md">
+      <DialogTitle>Edit Book</DialogTitle>
+      
+      <DialogContent>
+        <DialogContentText>
+          Fill the fields you want to change
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="title"
+          name='title'
+          label="Title"
+          type="text"
+          fullWidth
+          variant="outlined"
+          value={input.title}
+          onChange={(e)=>inputChange(e)}
+        />
+         <TextField
+          autoFocus
+          margin="dense"
+          id="author"
+          name="author"
+          label="Author"
+          type="text"
+          fullWidth
+          variant="outlined"
+          value={input.author}
+          onChange={(e)=>inputChange(e)}
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="editorial"
+          name="editorial"
+          label="Editorial"
+          type="text"
+          fullWidth
+          variant="outlined"
+          value={input.editorial}
+          onChange={(e)=>inputChange(e)}
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="year"
+          name="year"
+          label="Year"
+          type="number"
+          fullWidth
+          variant="outlined"
+          value={input.year}
+          onChange={(e)=>inputChange(e)}
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="price"
+          name="price"
+          label="Price"
+          type="number"
+          fullWidth
+          variant="outlined"
+          value={input.price}
+          onChange={(e)=>inputChange(e)}
+        />
+      </DialogContent>
+      
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="select-label-1" variant='outlined'>Category</InputLabel>
+        <Select
+          labelId="select-label-1"
+          id="select-1"
+          label="Category"
+          value={input.categorie}
+          onChange={(e) => selectCategorie(e)}
+        >
+          {allCategories?.map(cat => {return(<MenuItem key={cat} value={cat}>{cat}</MenuItem>)})}
+        </Select>
+      </FormControl>
+        
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="checkbox-label-1">Genders</InputLabel>
+        <Select
+          labelId="checkbox-label-1"
+          id="checkbox-1"
+          label="Genders"
+          input={<OutlinedInput label="Genders" />}
+          multiple
+          value={input.gender}
+          onChange={selectGendersMUI}
+          renderValue={(selected) => selected.join(', ')}
+        >
+          {allGenders?.map(gen => {return(
+          <MenuItem key={gen} value={gen}>
+            <Checkbox checked={input.gender.indexOf(gen) > -1} />
+            <ListItemText primary={gen} />
+          </MenuItem>
+          )})}
+        </Select>
+      </FormControl>
+      
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="select-label-2" variant='outlined'>Language</InputLabel>
+        <Select
+          labelId="select-label-2"
+          id="select-2"
+          label="Language"
+          value={input.language}
+          onChange={(e) => selectCategorie(e)}
+        >
+          {allLanguages?.map(cat => {return(<MenuItem key={cat} value={cat}>{cat}</MenuItem>)})}
+        </Select>
+      </FormControl>
+      
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <FormLabel id="radio-group-label-1">Type</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="radio-group-label-1"
+          name="typebook"
+          onChange={e=>changeStateMUI(e)}
+          value={input.typebook}
+        >
+          <FormControlLabel value="physical" control={<Radio />} label="Physical" />
+          <FormControlLabel value="virtual" control={<Radio />} label="Virtual" />
+        </RadioGroup>
+      </FormControl>
+      
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <FormLabel id="radio-group-label-2">State</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="radio-group-label-2"
+          name="state"
+          onChange={e=>changeStateMUI(e)}
+          value={input.state}
+        >
+          <FormControlLabel value="New" control={<Radio />} label="New" disabled={input.typebook === 'virtual'? (true) : (false)}/>
+          <FormControlLabel value="Used" control={<Radio />} label="Used" disabled={input.typebook === 'virtual'? (true) : (false)}/>
+        </RadioGroup>
+      </FormControl>
+      
+      
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={modifyMUI}>Modify</Button>
+      </DialogActions>
+    </Dialog>
+    
  </React.Fragment>
 )}
 
