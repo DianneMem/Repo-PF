f4add8c0600483041dddde4b544a5cc3f9c99502
DianneMem <102989674+DianneMem@users.noreply.github.com>
2022-12-28 21:18:55 -0300

    add styles header, search and detail

--- src/components/detail/detail.jsx
+++ src/components/detail/detail.jsx
@@ -4,8 +4,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { addFavorites, addStorage, addToCart, getBooksDetails, getAllUsers } from "../../redux/actions";
 import defaultImage from "../../assets/bookDefault.png";
-import "./detail.css";
-import favs from "../../assets/favs.png";
+import s from './detail.module.css'
+import fav from '../../assets/fav.png';
 import cart from "../../assets/cart.png";
 import ebook from "../../assets/ebook.png";
 import physical from "../../assets/physical.png";
@@ -13,6 +13,7 @@ import Header from "../header/Header";
 import Loader from "../loader/Loader";
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+
 
 export default function Detail() {
   let detail = useSelector((state) => state.detailsBook);
@@ -33,12 +34,12 @@ export default function Detail() {
     return elm._id === detail.sellerId
   })
 
-  
+
   useEffect(() => {
     dispatch(getBooksDetails(_id));
     dispatch(getAllUsers())
   }, [dispatch, _id]);
-  
+
 
 
   function backHandler(e) {
@@ -50,61 +51,63 @@ export default function Detail() {
     navigate(`/payment/${_id}`);
   }
 
-  function cartHandler(e,message) {
+  function cartHandler(e, message) {
     e.preventDefault();
-    if(!localStorage.getItem("cart")){
-      localStorage.setItem("cart","[]")
+    if (!localStorage.getItem("cart")) {
+      localStorage.setItem("cart", "[]")
     }
     let userId = JSON.parse(localStorage.getItem("session"));
-    if(userId){
-      let id=userId[0].id
-      if( getCart.filter(e=>e._id===detail._id).length<1){
+    if (userId) {
+      let id = userId[0].id
+      if (getCart.filter(e => e._id === detail._id).length < 1) {
         getCart.push(detail)
         localStorage.setItem("cart", JSON.stringify(getCart))
-        dispatch(addStorage(id,detail))}
-        else{
-          MySwal.fire('You already have this product in the cart!', message, 'error');
-        }
-      
-    console.log(getCart);
-    
-  }else{
-    if(!localStorage.getItem("cart")){
-      localStorage.setItem("cart","[]")
+        dispatch(addStorage(id, detail))
+      }
+      else {
+        MySwal.fire('You already have this product in the cart!', message, 'error');
+      }
+
+      console.log(getCart);
+
+    } else {
+      if (!localStorage.getItem("cart")) {
+        localStorage.setItem("cart", "[]")
+      }
+      let getCart = JSON.parse(localStorage.getItem("cart"));
+      getCart.filter(e => e._id === detail._id).length < 1 ? getCart.push(detail) : MySwal.fire('You already have this product in the cart!', message, 'error');
+      localStorage.setItem("cart", JSON.stringify(getCart))
     }
-    let getCart = JSON.parse(localStorage.getItem("cart"));
-    getCart.filter(e=>e._id===detail._id).length<1 ? getCart.push(detail):MySwal.fire('You already have this product in the cart!', message, 'error');
-    localStorage.setItem("cart", JSON.stringify(getCart))
-  }
-    
+
   }
 
-  function favsHandler(e,message){
+  function favsHandler(e, message) {
     e.preventDefault();
-    if(!localStorage.getItem("favs")){
-      localStorage.setItem("favs","[]")
+    if (!localStorage.getItem("favs")) {
+      localStorage.setItem("favs", "[]")
     }
     let favs = JSON.parse(localStorage.getItem("favs"));
     let userId = JSON.parse(localStorage.getItem("session"));
-    if(userId){
-      let id=userId[0].id
-    if(favs.filter(e=>e._id===detail._id).length<1){
-      favs.push(detail)
-      localStorage.setItem("favs", JSON.stringify(favs))
-dispatch(addFavorites(id,detail))}
-else{
-  MySwal.fire('You already have this product in the cart!', message, 'error')
-}
-    
-  }else{
-    if(!localStorage.getItem("favs")){
-      localStorage.setItem("favs","[]")
-     
-    }
+    if (userId) {
+      let id = userId[0].id
+      if (favs.filter(e => e._id === detail._id).length < 1) {
+        favs.push(detail)
+        localStorage.setItem("favs", JSON.stringify(favs))
+        dispatch(addFavorites(id, detail))
+      }
+      else {
+        MySwal.fire('You already have this product in the cart!', message, 'error')
+      }
 
-    MySwal.fire('Please register to be able to add products to Favorites!', message, 'info')
-    navigate("/login");
-  }
+    } else {
+      if (!localStorage.getItem("favs")) {
+        localStorage.setItem("favs", "[]")
+
+      }
+
+      MySwal.fire('Please register to be able to add products to Favorites!', message, 'info')
+      navigate("/login");
+    }
 
 
   }
@@ -118,127 +121,124 @@ else{
   if (!loading) {
     changeState();
     return <Loader />;
-  } 
-  
+  }
+
   const score = user.reviews.map((elm) => {
     return elm.score
   })
 
-  
+
   return (
-    <div className="contain"  style={{ "background-color": theme && "#212529", "color": theme && "white"}}>
+    <div className={s.contain} style={{ "background-color": theme && "#212529", "color": theme && "white" }}>
       <Header noSearch={true} />
 
-      <div class="details">
-        <button onClick={(e) => backHandler(e)} className="buttonBack">
-          ←
-        </button>
-        <div class="left">
-          <div className="imgCard">
-            <img
-              src={detail.image ? detail.image : defaultImage}
-              alt="Book"
-              className="image"
-            />
-            <h2>Seller: {detail.seller}</h2>
-            {
-              !score.length? 
-              <div></div>:
-            <h3>Score:{score.reduce((acc,curr) => acc + curr)/5}</h3>
-            }
-            <h4>Reviews</h4>
-            {
-              !user.reviews.length?
-              <a>There are no reviews</a>:
-              user.reviews.map((elm) => {
-                return(
-                  <p>{elm.sellerId}: {elm.comment}</p>
-                )
-              })
-            }
-          </div>
+      {/* 
+      <button onClick={(e) => backHandler(e)} className="buttonBack">
+        ←
+      </button> */}
+      <div className={s.left}>
+        <div className="imgCard">
+          <img
+            src={detail.image ? detail.image : defaultImage}
+            alt="Book"
+            width={"100%"}
+          />
         </div>
-        <div class="right">
-          <div className="cardDetail">
-            <h1 className="titleDetail">{detail.title}</h1>
-            <div className="labelDetail">
-              <p class="">
-                Author: <span className="detailSpan">{detail.author}</span>{" "}
-              </p>
-              <p class="">
-                Category:{" "}
-                <span className="detailSpan"> {detail.categorie}</span>
-              </p>
-              <p class="">
-                Editorial:{" "}
-                <span className="detailSpan">{detail.editorial}</span>{" "}
-              </p>
-              {detail.saga && (
-                <p class="">
-                  Saga: <span className="detailSpan">{detail.saga}</span>{" "}
-                </p>
-              )}
-              <p>
-                Lenguage: <span className="detailSpan">{detail.language}</span>
-              </p>
-              <p>
-                Gender:{" "}
-                <span className="detailSpan">
-                  {detail.gender?.map((e) => {
-                    return <span>{e}</span>;
-                  })}{" "}
-                </span>{" "}
-              </p>
-              <p>
-                Year: <span className="detailSpan"> {detail.year}</span>{" "}
-              </p>
-              <p>
-                State: <span className="detailSpan"> {detail.state}</span>{" "}
-              </p>
-              <div class="top">
-                <div>
-                  <a href="#">
-                    <button onClick={(e) => favsHandler(e)}>    
-                       <img className="icon" src={favs} alt="" />
-                    </button>
-               
-                  </a>
-                  <a href="#">
-                  <button onClick={(e) => cartHandler(e)} className="button">
-                  <img className="icon" src={cart} alt="" />
-                  </button>
-                  
-                  </a>
-                </div>
-              </div>
-              {/* <p>Available: {detail.available}</p> */}
-              <div class="comprar">
-                <div class="comprar-left">
-                  {detail.typebook && detail.typebook == "virtual" ? (
-                    <>
-                      <img src={ebook} alt="imageD" />
-                      <p>ebook</p>
-                    </>
-                  ) : (
-                    <>
-                      <img src={physical} alt="imageDet" />
-                      <p>Physical book</p>
-                    </>
-                  )}
-                </div>
-                <div class="comprar-right">
-                  <h3>{detail.price}</h3>
 
-                  <button onClick={(e) => paymentHandler(e)} className="button">
-                    Buy
-                  </button>
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
+
+      <div className={s.right}>
+        <div className={s.cardDetail}>
+          <h1 className={s.titleDetail}>{detail.title}</h1>
+          <div className={s.detail}>
+            <p class="">
+              Author: <span className={s.detail}>{detail.author}</span>{" "}
+            </p>
+            <p class={s.detail}>
+              Category:{" "}
+              <span className={s.detail}> {detail.categorie}</span>
+            </p>
+            <p className={s.detail} >
+              Editorial:{" "}
+              <span className={s.detail}>{detail.editorial}</span>{" "}
+            </p>
+            {detail.saga && (
+              <p class="">
+                Saga: <span className={s.detail}>{detail.saga}</span>{" "}
+              </p>
+            )}
+            <p>
+              Lenguage: <span className={s.detail}>{detail.language}</span>
+            </p>
+            <p>
+              Gender:{" "}
+              <span className={s.detail}>
+                {detail.gender?.map((e) => {
+                  return <span>{e}</span>;
+                })}{" "}
+              </span>{" "}
+            </p>
+            <p>
+              Year: <span className={s.detail}> {detail.year}</span>{" "}
+            </p>
+            <p>
+              State: <span className={s.detail}> {detail.state}</span>{" "}
+            </p>
+          </div>
+
+          {/* <p>Available: {detail.available}</p> */}
+          <div className={s.typebook}>
+            {detail.typebook && detail.typebook == "virtual" ? (
+              <>
+                <img src={ebook} alt="imageD" />
+                <p>ebook</p>
+              </>
+            ) : (
+              <>
+                <img src={physical} alt="imageDet" />
+                <p>Physical book</p>
+              </>
+            )}
+          </div>
+
+          <h3>$ {detail.price}</h3>
+
+          <div className={s.buttonsIcons}>
+            <button onClick={(e) => favsHandler(e)}>
+              <img className={s.icon} src={fav} alt="" />
+            </button>
+
+            <button onClick={(e) => cartHandler(e)} className="button">
+              <img className={s.icon} src={cart} alt="" />
+            </button>
+          </div>
+
+          <button onClick={(e) => paymentHandler(e)} className={s.button}>
+            Buy
+          </button>
+        </div>
+
+      </div>
+      <div className={s.detailSeller}>
+
+        <h2>Seller: {detail.seller}</h2>
+        {
+          !score.length ?
+            <div></div> :
+            <h3>Score:{score.reduce((acc, curr) => acc + curr) / 5}</h3>
+        }
+        <h4>Reviews</h4>
+        {
+          !user.reviews.length ?
+            <a>There are no reviews</a> :
+            user.reviews.map((elm) => {
+              return (
+                <p>{elm.sellerId}: {elm.comment}</p>
+              )
+            })
+        }
+      </div>
+
     </div>
   );
 }
