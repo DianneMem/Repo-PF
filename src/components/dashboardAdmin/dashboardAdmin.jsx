1ecf63b2238b4c340b43a099449c220c4c70a9fc
lucasbeck1 <lucasbeckford1@gmail.com>
2022-12-29 23:36:23 -0300

    feat/ refactor Admin Products

--- src/components/dashboardAdmin/dashboardAdmin.jsx
+++ src/components/dashboardAdmin/dashboardAdmin.jsx
@@ -66,30 +66,15 @@ export default function DashAdmin() {
     e.preventDefault();
     dispatch(getAllBooks());
     setCurrentPage(1);
-  }
+  };
 
   function changePage(e) {
     e.preventDefault();
-    if (e.target.value === "less" && currentPage !== 1) {
-      setCurrentPage(currentPage - 1);
-    } else if (e.target.value === "more" && currentPage !== pages) {
-      setCurrentPage(currentPage + 1);
-    }
-  }
+    if (e.target.value === "less" && currentPage !== 1) setCurrentPage(currentPage - 1);
+    else if (e.target.value === "more" && currentPage !== pages) setCurrentPage(currentPage + 1);
+  };
 
-  async function disableItem(e){
-    e.preventDefault();
-    let itemId = e.target.value;
-    await dispatch(disablePost(itemId));
-    dispatch(getAllBooks());
-  }
-  
-  async function deleteItem(e){
-    e.preventDefault();
-    let itemId = e.target.value;
-    await dispatch(deletePost(itemId));
-    dispatch(getAllBooks());
-  }
+
   
   function handleSections(e){
     e.preventDefault();
@@ -133,9 +118,9 @@ export default function DashAdmin() {
       
       
     <div className={s.page}>
-  
       <nav className={s.nav_side}>
         <ul className={s.nav_container}>
+        
           <li className={s.nav_items}>
             <svg viewBox="0 0 20 20">
 							<path d="M18.121,9.88l-7.832-7.836c-0.155-0.158-0.428-0.155-0.584,0L1.842,9.913c-0.262,0.263-0.073,0.705,0.292,0.705h2.069v7.042c0,0.227,0.187,0.414,0.414,0.414h3.725c0.228,0,0.414-0.188,0.414-0.414v-3.313h2.483v3.313c0,0.227,0.187,0.414,0.413,0.414h3.726c0.229,0,0.414-0.188,0.414-0.414v-7.042h2.068h0.004C18.331,10.617,18.389,10.146,18.121,9.88 M14.963,17.245h-2.896v-3.313c0-0.229-0.186-0.415-0.414-0.415H8.342c-0.228,0-0.414,0.187-0.414,0.415v3.313H5.032v-6.628h9.931V17.245z M3.133,9.79l6.864-6.868l6.867,6.868H3.133z"></path>
@@ -168,29 +153,19 @@ export default function DashAdmin() {
       </nav>
 
       
-      
-      
-      
-      
-  
-      
+
       
       
       <div className={s.content}>
      
       {section === 'Products' && <>
       
-
       <SideBar vertical={false}/>
-      
-      
-      
+    
       {createProduct &&
       (<Modal open={createProduct}>
-        <div className={s.createPost}>
-        <button  onClick={e => handleCreateProduct(e)}>Close Form</button>
+          <button  onClick={e => handleCreateProduct(e)}>Close Form</button>
           <CreatePost/>
-        </div>
         </Modal>) 
       }
       
@@ -247,19 +222,10 @@ export default function DashAdmin() {
                 year={b.year}
                 state={b.state}
                 available={b.available}
-                disable={disableItem}
-                deletes={deleteItem}
                 />
               </div>)
             })}
           </div>
-          
-          
-          
-          
-          
-          
-          
 
         </div>) :
         (<>
